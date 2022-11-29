0d954d285654d0b26b6628dcaaa5d623c0cb9c35
Hala <83414449+Hala-Salah@users.noreply.github.com>
2022-11-30 01:26:34 +0200

    About Matter is done

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Carousels from '../components/HomePage/Carousels'
+import React from "react";
+import AboutMatter from "../components/HomePage/AboutMatter";
+import Carousels from "../components/HomePage/Carousels";
 
 const HomePage = () => {
   return (
-    <><Carousels/></>
-  )
-}
+    <>
+      <Carousels />
+      <AboutMatter />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
