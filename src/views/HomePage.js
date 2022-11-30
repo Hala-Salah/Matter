d6f19ef47f3919cae95670feed92fb9a18dd5bfd
Hala <83414449+Hala-Salah@users.noreply.github.com>
2022-11-30 22:45:30 +0200

    Recommended Videos is done

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AboutMatter from "../components/HomePage/AboutMatter";
 import Carousels from "../components/HomePage/Carousels";
+import RecommendedVideos from "../components/HomePage/Recommended Videos";
 
 const HomePage = () => {
   return (
     <>
       <Carousels />
+      
       <AboutMatter />
+      <RecommendedVideos/>
     </>
   );
 };
